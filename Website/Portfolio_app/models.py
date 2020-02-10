7670aa05458c31c39de37c095dc5cfa512f793dd
aayu0795 <aayushkumar0795@gmail.com>
2020-02-10 21:30:34 +0530

    Initial commit

---
+++ Website/Portfolio_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=1000)
+    description = models.TextField(max_length=10000)
+    technology_used = models.CharField(max_length=1000)
+    thumbnail = models.ImageField(upload_to='pics')
+    link = models.CharField(max_length=1000)
---
+++ Website/Portfolio_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Project
+
+# Create your views here.
+
+
+def index(request):
+    projects = Project.objects.all()
+    return render(request, 'index.html', {'projects': projects})
+
+
+def project(request):
+    return render(request, 'project.html')
