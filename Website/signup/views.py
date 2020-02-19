848eb7649d1fd79a63fed87db929f024920605dc
aayu0795 <aayushkumar0795@gmail.com>
2020-02-19 09:26:05 +0530

    19/02/2020 9:25AM

--- Website/signup/views.py
+++ Website/signup/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -16,12 +17,19 @@ def signup(request):
 
         if password == confirm_password:
 
-            user = User.objects.create_user(username=username, password=password,
-                                            email=email, first_name=first_name, last_name=last_name)
-            user.save()
-            return redirect('/')
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "Username already exists")
+
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "Email already exists")
+
+            else:
+                user = User.objects.create_user(username=username, password=password,
+                                                email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                return redirect('/')
 
         else:
-            print("Password not matching")
+            messages.info(request, "Password not matching")
 
     return render(request, 'signup.html')
